47282f8cbc459626327ca240c5d7491cacaca99b
venkatesh901 <venkatesh.yadam@eagles.oc.edu>
2016-08-25 16:14:58 -0500

    Add files via upload
    commit

---
+++ PlayList.java
@@ -0,0 +1,33 @@
+package com.playlist.pojo;
+
+public class PlayList {
+
+	private String name;
+	private int duration;
+	private String type;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}
---
+++ Tracks.java
@@ -0,0 +1,114 @@
+package com.playlist.pojo;
+
+public class Tracks {
+
+	private int trackid;
+	private int totaltime;
+	private int discnumber;
+	private int disccount;
+	private int tracknumber;
+	private int trackcount;
+	private int year;
+	private String artist;
+	private String album;
+	private String genre;
+	private String name;
+	private String albumartist;
+
+	public String getAlbumartist() {
+		return albumartist;
+	}
+
+	public void setAlbumartist(String albumartist) {
+		this.albumartist = albumartist;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getTrackid() {
+		return trackid;
+	}
+
+	public void setTrackid(int trackid) {
+		this.trackid = trackid;
+	}
+
+	public int getTotaltime() {
+		return totaltime;
+	}
+
+	public void setTotaltime(int totaltime) {
+		this.totaltime = totaltime;
+	}
+
+	public int getDiscnumber() {
+		return discnumber;
+	}
+
+	public void setDiscnumber(int discnumber) {
+		this.discnumber = discnumber;
+	}
+
+	public int getDisccount() {
+		return disccount;
+	}
+
+	public void setDisccount(int disccount) {
+		this.disccount = disccount;
+	}
+
+	public int getTracknumber() {
+		return tracknumber;
+	}
+
+	public void setTracknumber(int tracknumber) {
+		this.tracknumber = tracknumber;
+	}
+
+	public int getTrackcount() {
+		return trackcount;
+	}
+
+	public void setTrackcount(int trackcount) {
+		this.trackcount = trackcount;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+	public String getAlbum() {
+		return album;
+	}
+
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+}
